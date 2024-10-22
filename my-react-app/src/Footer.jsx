d8d7b79af756c6e9c5a1409b5d8712d65da3c0f5
matthewfaler <matthewfaler1@gmail.com>
2024-10-22 15:12:19 -0400

    Made skill cards float left and take up half of parent element

--- my-react-app/src/Footer.jsx
+++ my-react-app/src/Footer.jsx
@@ -1,3 +1,4 @@
+
 function Footer() {
     const birthDate = new Date(1998, 10, 14, 3, 6).getTime();
     const currentDate = new Date().getTime();
--- my-react-app/src/SkillDiv/SkillCard.jsx
+++ my-react-app/src/SkillDiv/SkillCard.jsx
@@ -1,8 +1,8 @@
-
+import styles from './SkillCard.module.css'
 
 function SkillCard({skill}) {
     return(
-        <div className="skill-card">
+        <div className={styles.SkillCard}>
             <h3>{skill.title}</h3>
             <p>{skill.description}</p>
         </div>
