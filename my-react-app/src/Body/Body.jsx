d0a0cad103836795fac8bf03c2656bb8eaa82662
matthewfaler <matthewfaler1@gmail.com>
2024-10-23 22:28:08 -0400

    Added that some files were deleted to git commit

--- my-react-app/src/Body/Body.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './Body.module.css';
-
-const Body = ({text}) => {
-    return(
-        <p className={styles.Body}>{text}</p>
-    );
-}
-export default Body
--- my-react-app/src/Header/Header.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './Header.module.css';
-
-const Header = ({text}) => {
-    return(
-        <h1 className={styles.Header}>{text}</h1>
-    );
-}
-export default Header
