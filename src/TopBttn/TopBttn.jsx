af2f362bf6eae813e47045a4e36fae92f1a1d382
matthewfaler <matthewfaler1@gmail.com>
2025-07-04 11:33:32 -0400

    refs #16: added working scroll to top button that hides itself

--- src/TopBttn/TopBttn.jsx
+++ src/TopBttn/TopBttn.jsx
@@ -1,4 +1,5 @@
-import styles from './TopBttn.module.css'
+import { useState, useEffect } from 'react';
+import styles from './TopBttn.module.css';
 
 
 function ScrollToTop() {
@@ -8,8 +9,22 @@ function ScrollToTop() {
     });
 }
 function TopBttn() {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsVisible(window.scrollY > 200)
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
     return (
-        <button className={styles.TopBttn} onClick={ScrollToTop}>Scroll to Top</button>
+        <button className={`${styles.TopBttn} ${isVisible ? styles.visible : styles.hidden}`} onClick={ScrollToTop}>Scroll to Top</button>
     );
 }
 export default TopBttn
